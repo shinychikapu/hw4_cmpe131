5d71e66ee7ebd2cb38a09d73902321407a35068e
Gia Thy Le <giathy2843@gmail.com>
2023-10-09 22:15:37 -0700

    fixed my steps

--- ladder.py
+++ ladder.py
@@ -1,7 +1,9 @@
-def my_step(n):
-    if n == 1 or n == 0:
-        return '1 step '
+def my_steps(n):
+    if not isinstance(n, int):
+        raise ValueError
+    if n == 1 or n == 2:
+        return n
     else:
-        return my_step(n-1) + my_step(n-2)
-print(my_step(5)) 
+        return my_steps(n-1) + my_steps(n-2)
 
+print(my_steps(3))
