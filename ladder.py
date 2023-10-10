5931d49ef8214e430269f4740e2fc6a6c92456ca
Gia Thy Le <giathy2843@gmail.com>
2023-10-09 22:19:49 -0700

    fixed ladder

--- ladder.py
+++ ladder.py
@@ -1,9 +1,9 @@
 def my_steps(n):
-    if not isinstance(n, int) and not (1 <= n <= 25):
+    if not isinstance(n, int) or not (1 <= n <= 25):
         raise ValueError
     if n == 1 or n == 2:
         return n
     else:
         return my_steps(n-1) + my_steps(n-2)
 
-print(my_steps(2))
+print(my_steps(0))
